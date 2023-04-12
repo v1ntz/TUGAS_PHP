833ddb806511951b61c3a33f099a59aa30924a2e
v1ntz <avtamvan@gmail.com>
2023-04-12 16:57:56 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CV - Albert Vincentius Siahaan</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Curriculum Vitae</h1>
+        <hr class="title-line">
+        <div class="section">
+            <h2>Data Pribadi</h2>
+            <hr class="section-line">
+            <div class="data-pribadi">
+                <div class="profile-img">
+                    <img src="PP CSS.jpg" alt="Profile Photo">
+                </div>
+                <ul>
+                    <li><strong>Nama:</strong> Albert Vincentius Siahaan</li>
+                    <li><strong>TTL:</strong> Jakarta, 16 Maret 2003</li>
+                    <li><strong>Umur:</strong> 20 </li>
+                    <li><strong>Jenis Kelamin:</strong> Laki-Laki</li>
+                    <li><strong>Agama:</strong> Katholik</li>
+                    <li><strong>Email:</strong> avtamvan@gmail.com</li>
+                    <li><strong>No.HP:</strong> 0899-0676-940</li>
+                </ul>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Data Pendidikan</h2>
+            <hr class="section-line">
+            <div class="data-pendidikan">
+                <ul>
+                    <li><strong>SD:</strong> SDN 18 Aren Jaya (2009-2015)</li>
+                    <li><strong>SMP:</strong> SMPN 6 Tambun Selatan (2015-2018)</li>
+                    <li><strong>SMA:</strong> SMAN 8 Tambun Selatan (2018-2021)</li>
+                    <li><strong>Perguruan Tinggi:</strong> UNIVERSITAS PEMBANGUNAN NASIONAL "VETERAN" JAWA TIMUR (2021-Sekarang)</li>
+                </ul>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Kemampuan</h2>
+            <hr class="section-line">
+            <div class="kemampuan">
+                <ul>
+                    <li><strong>Informasi Teknologi:</strong> HTML, CSS, C++, Python</li>
+                    <li><strong>Bahasa:</strong> Bahasa Indonesia (Aktif), English (Pasif)</li>
+                </ul>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Pengalaman</h2>
+            <hr class="section-line">
+            <div class="pengalaman">
+                <ul>
+                    <li>Fresh Graduate</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
